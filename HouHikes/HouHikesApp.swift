1dcb6a6b6de781f8a9d029fa293680964430d2f8
Jorge Pinto <jorgepintovega97@gmail.com>
2024-07-07 15:56:18 -0500

    Initial Commit

---
+++ HouHikes/HouHikesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HouHikesApp.swift
+//  HouHikes
+//
+//  Created by Jorge Pinto on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct HouHikesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
